3fb64e6c68e32c187ec29a27c20fe99a17cdf6fa
jessicagmarshall <jessica.marshall14@gmail.com>
2018-12-03 10:48:01 -0500

    reward task button shows up properly on vote page where appropriate

--- frontend/src/contractComponents/stage5/RewardTask.js
+++ frontend/src/contractComponents/stage5/RewardTask.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Button } from 'antd'
 import { rewardTask } from '../../actions/taskActions'
-import { eth } from '../../utilities/blockchain'
 
 const ButtonRewardTask = (props) => {
   return (<Button
-    disabled={props.tasks[props.i].claimer.account !== eth.accounts[0] || props.tasks[props.i].workerRewarded}
+    disabled={props.tasks[props.i].claimer.account !== props.user || props.tasks[props.i].workerRewarded}
     type='danger'
     onClick={() => props.rewardTask(props.address, props.i, {from: props.user}, props.state)}>
     Reward Task
