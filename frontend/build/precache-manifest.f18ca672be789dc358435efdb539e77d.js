7ae060fddc2f578777eeb816b2af01d20d50faae
jessicagmarshall <jessica.marshall14@gmail.com>
2018-12-04 13:04:42 -0500

    ran yarn build and deployed to netlify

---
+++ frontend/build/precache-manifest.f18ca672be789dc358435efdb539e77d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db38b6eb7924dbcebf50",
+    "url": "/static/css/main.8afc4ea6.chunk.css"
+  },
+  {
+    "revision": "db38b6eb7924dbcebf50",
+    "url": "/static/js/main.db38b6eb.chunk.js"
+  },
+  {
+    "revision": "15a6725c1cc260b107b9",
+    "url": "/static/css/1.a5adb206.chunk.css"
+  },
+  {
+    "revision": "15a6725c1cc260b107b9",
+    "url": "/static/js/1.15a6725c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "29fc17f1440f71822d1aaaa190326ef9",
+    "url": "/index.html"
+  }
+];
